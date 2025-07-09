c5b4e9d4ac660ae566c9b709bfac9190390a8508
tcspariwesh <tcspariwesh@gmail.com>
2025-07-09 19:55:55 +0530

    after init command

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
+import { withGluestackUI } from "@gluestack/ui-next-adapter";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["nativewind", "react-native-css-interop"]
 };
 
-export default nextConfig;
+export default withGluestackUI(nextConfig);
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { flush } from "@gluestack-ui/nativewind-utils/flush";
+import { AppRegistry } from "react-native-web";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -11,3 +14,10 @@ export default function Document() {
     </Html>
   );
 }
+Document.getInitialProps = async ({ renderPage }) => {
+  AppRegistry.registerComponent("Main", () => Main);
+  const { getStyleElement } = AppRegistry.getApplication("Main");
+  const page = await renderPage();
+  const styles = [getStyleElement(), flush()];
+  return { ...page, styles: React.Children.toArray(styles) };
+};
